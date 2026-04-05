02e1c65b91237febd02eedc011d7588dbe6f44b6
PokipokiBagre <amanojaku344@gmail.com>
2026-04-04 20:59:40 -0500

    Create panel-stats-state.js

---
+++ panel-de-desarrollo/estadisticas/panel-stats-state.js
@@ -0,0 +1,17 @@
+// ============================================================
+// panel-stats-state.js
+// ============================================================
+
+export const stState = {
+    // Variables obligatorias que busca dev-logic.js
+    statsDB: {},
+    estadosDB: [],
+    colaStats: {},
+    colaNotas: {},
+    colaEstadosConfig: {},
+    colaBorrarEstados: [],
+
+    // Variables para el sistema de agrupación de puntos
+    personajesRaw: [],
+    personajesRefinados: []
+};
