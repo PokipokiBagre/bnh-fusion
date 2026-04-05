0b101e7c8f4a25688a36f000eb921796e02648d6
PokipokiBagre <amanojaku344@gmail.com>
2026-04-04 22:34:38 -0500

    Update dev-logic.js

--- panel-de-desarrollo/dev-logic.js
+++ panel-de-desarrollo/dev-logic.js
@@ -2,13 +2,6 @@
 // dev-logic.js — Lógica Centralizada (Simplificada)
 // ============================================================
 
-import { stState } from './estadisticas/panel-stats-state.js';
-import { paginaState } from './pagina/panel-pagina-state.js';
-
-// Si en el futuro necesitas lógicas globales cruzadas entre 
-// la página y las agrupaciones, irán aquí. 
-// Por ahora, cada módulo se auto-gestiona de manera limpia.
-
 export function limpiarSeleccion() {
     console.log("Limpiando selección global...");
 }
